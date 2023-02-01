bfacf5dc96bea970d9c78d9aeaad5f83e6a4e23e
Sayem Mohammed Al-Mahdi <sayemalmahdi@gmail.com>
2023-02-02 00:20:02 +0600

    MongoDB Atlas & Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false);
+
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -17,7 +20,26 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
   
-// listen for requests
-app.listen(process.env.PORT, () => {
-   console.log('listening on port', process.env.PORT);
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
+
+// I don't know how it works but I had to write the same code 2 times (connect to db) and then it works
+
+  mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
